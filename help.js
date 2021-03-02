b718079a667cdda328153f621b14426696dfac9d
var-syn <79808604+var-syn@users.noreply.github.com>
2021-03-02 12:02:43 +0000

    Removed background
    removed header background from javacsript file

---
+++ help.js
@@ -0,0 +1,185 @@
+
+
+
+function imagefunction() {alert('im var_syn.... Hello')}
+function imageFunction(){alert('im skinypig...Hello')}
+function IMAGEFUNCTION(){alert('im gbourgeo...Hello')}
+function imagee(){alert('im qbert...Hello')}
+function imge(){alert('madspank')}
+function ermage(){alert('im DMCFA...Hello')}
+function eeeemage(){alert('im cobalt... hello')}
+var image = document.getElementById("image")
+image.addEventListener("mouseover", function(){
+	this.style ="box-shadow: 2px 2px 2px grey";
+	this.width = "110"
+	this.height= "110"
+});
+image.addEventListener("mouseout", function(){
+	this.style="";
+	this.width="100"
+	this.height= "100"
+})
+
+var image1 = document.getElementById("skinypig")
+image1.addEventListener("mouseover", function(){
+	this.style = "box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+
+image1.addEventListener("mouseout", function(){
+	this.style= ""
+	this.width="100"
+	this.height="100"
+})
+
+var image2 = document.getElementById("gbourgeo")
+image2.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image2.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
+var image3 = document.getElementById("qbert")
+image3.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image3.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+var image4 = document.getElementById("madspank")
+image4.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image4.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+var image5 = document.getElementById("DMCFA")
+image5.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image5.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
+var image6 = document.getElementById("cobalt")
+image6.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image6.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
+
+
+function imagefunction() {alert('im var_syn.... Hello')}
+function imageFunction(){alert('im skinypig...Hello')}
+function IMAGEFUNCTION(){alert('im gbourgeo...Hello')}
+function imagee(){alert('im qbert...Hello')}
+function imge(){alert('madspank')}
+function ermage(){alert('im DMCFA...Hello')}
+function eeeemage(){alert('im cobalt... hello')}
+var image = document.getElementById("image")
+image.addEventListener("mouseover", function(){
+	this.style ="box-shadow: 2px 2px 2px grey";
+	this.width = "110"
+	this.height= "110"
+});
+image.addEventListener("mouseout", function(){
+	this.style="";
+	this.width="100"
+	this.height= "100"
+})
+
+var image1 = document.getElementById("skinypig")
+image1.addEventListener("mouseover", function(){
+	this.style = "box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+
+image1.addEventListener("mouseout", function(){
+	this.style= ""
+	this.width="100"
+	this.height="100"
+})
+
+var image2 = document.getElementById("gbourgeo")
+image2.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image2.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
+var image3 = document.getElementById("qbert")
+image3.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image3.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+var image4 = document.getElementById("madspank")
+image4.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image4.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+var image5 = document.getElementById("DMCFA")
+image5.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image5.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
+var image6 = document.getElementById("cobalt")
+image6.addEventListener("mouseover", function(){
+	this.style="box-shadow: 2px 2px 2px grey";
+	this.width="110"
+	this.height="110"
+})
+image6.addEventListener("mouseout", function(){
+	this.style=""
+	this.width="100"
+	this.height="100"
+})
+
