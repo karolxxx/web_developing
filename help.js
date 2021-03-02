d5381d0d0ca874e87bba3474177a66c7ec050ebc
var-syn <79808604+var-syn@users.noreply.github.com>
2021-03-02 12:51:41 +0000

    put an effect on llinks and changed some ids

--- help.js
+++ help.js
@@ -183,3 +183,30 @@ image6.addEventListener("mouseout", function(){
 	this.height="100"
 })
 
+var link = document.getElementById=("link1")
+link1.addEventListener("mouseover", function(){
+	this.style.color="red"
+	
+})
+link1.addEventListener("mouseout", function(){
+	this.style.color="white"
+	
+})
+var link = document.getElementById=("link2")
+link2.addEventListener("mouseover", function(){
+	this.style.color="red"
+	
+})
+link2.addEventListener("mouseout", function(){
+	this.style.color="white"
+	
+})
+var link = document.getElementById=("link3")
+link3.addEventListener("mouseover", function(){
+	this.style.color="red"
+	
+})
+link3.addEventListener("mouseout", function(){
+	this.style.color="white"
+	
+})
