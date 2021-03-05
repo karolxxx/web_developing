84035fe6f0d69f2d3971521d15b2d598c98dae24
var-syn <79808604+var-syn@users.noreply.github.com>
2021-03-05 10:39:37 +0000

    added JS to nav bar
    can tweak if needed let me know

--- JS/new.js
+++ JS/new.js
@@ -1,28 +1,28 @@
 var link = document.getElementById=("link4")
 link4.addEventListener("mouseover", function(){
-	this.style.color="red"
+	this.style.color="rgb(87, 189, 189)"
 	
 })
 link4.addEventListener("mouseout", function(){
-	this.style.color="white"
+	this.style.color="rgb(59, 63, 71)"
 	
 })
 var link = document.getElementById=("link5")
 link5.addEventListener("mouseover", function(){
-	this.style.color="red"
+	this.style.color="rgb(87, 189, 189)"
 	
 })
 link5.addEventListener("mouseout", function(){
-	this.style.color="white"
+	this.style.color="rgb(59, 63, 71)"
 	
 })
 var link = document.getElementById=("link6")
 link6.addEventListener("mouseover", function(){
-	this.style.color="red"
+	this.style.color="rgb(87, 189, 189)"
 	
 })
 link6.addEventListener("mouseout", function(){
-	this.style.color="white"
+	this.style.color="rgb(59, 63, 71)"
 	
 
 })
